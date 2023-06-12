968a1f7b97a926969a2e56df07b552c903a289b2
Matt Payne <Payne@MattPayne.org>
2023-06-11 21:48:46 -0500

    Silly little components

---
+++ src/NoRoles.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function NoRoles() {
+
+  return (
+    <>
+	<h1>NoRoles.tsx will show people without jobs</h1>
+    </>
+  )
+}
+
+export default NoRoles
---
+++ src/Roles.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function Roles() {
+
+  return (
+    <>
+	<h1>Roles.tsx</h1>
+    </>
+  )
+}
+
+export default Roles
